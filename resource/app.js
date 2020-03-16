db0263b967ff5629a3695a5719d57425d7797ba6
jr638091 <jr638091@gmail.com>
2020-03-16 12:02:10 -0400

    edit file finished

--- resource/app.js
+++ resource/app.js
@@ -14,68 +14,64 @@ var access_token;
 
 OAuth.popup('github').done(function (result) {
   access_token = result.access_token;
-});
+  $.getJSON(`http://api.github.com/repos/${owner}/${repo}/contents/resource/dataset.json`, { Authorization: `${access_token} OAUTH-TOKEN` }).done(function (data) {
+    dataset_info = data;
+    // Initialize the editor
+    editor = new JSONEditor(document.getElementById('editor_holder'), {
+      // Enable fetching schemas via ajax
+      ajax: true,
+      theme: 'bootstrap4',
+      // The schema for the editor
+      schema: {
+        type: "array",
+        title: "People",
+        format: "tabs",
+        items: {
+          title: "Person",
+          headerTemplate: "{{i}} - {{self.name}}",
+          oneOf: [
+            {
+              $ref: "resource/basic_person.json",
+              title: "Basic Person"
+            },
+            {
+              $ref: "resource/person.json",
+              title: "Complex Person"
+            }
+          ]
+        }
+      },
 
-$.getJSON(`http://api.github.com/repos/${owner}/${repo}/contents/resource/dataset.json`,{Authorization: `${access_token} OAUTH-TOKEN`}).done( function (data) {
-  dataset_info = data;
-  // Initialize the editor
-  editor = new JSONEditor(document.getElementById('editor_holder'), {
-    // Enable fetching schemas via ajax
-    ajax: true,
-    theme: 'bootstrap4',
-    // The schema for the editor
-    schema: {
-      type: "array",
-      title: "People",
-      format: "tabs",
-      items: {
-        title: "Person",
-        headerTemplate: "{{i}} - {{self.name}}",
-        oneOf: [
-          {
-            $ref: "resource/basic_person.json",
-            title: "Basic Person"
-          },
-          {
-            $ref: "resource/person.json",
-            title: "Complex Person"
-          }
-        ]
+      // Seed the form with a starting value
+      startval: JSON.parse(atob(data.content)),
+
+      // Disable additional properties
+      no_additional_properties: true,
+
+      // Require all properties by default
+      required_by_default: true
+    });
+    // Hook up the validation indicator to update its 
+    // status whenever the editor changes
+    editor.on('change', function () {
+      // Get an array of errors from the validator
+      var errors = editor.validate();
+
+      var indicator = document.getElementById('valid_indicator');
+
+      // Not valid
+      if (errors.length) {
+        indicator.style.color = 'red';
+        indicator.textContent = "not valid";
       }
-    },
-
-    // Seed the form with a starting value
-    startval: JSON.parse(atob(data.content)),
-
-    // Disable additional properties
-    no_additional_properties: true,
-
-    // Require all properties by default
-    required_by_default: true
-  });
-  // Hook up the validation indicator to update its 
-  // status whenever the editor changes
-  editor.on('change', function () {
-    // Get an array of errors from the validator
-    var errors = editor.validate();
-
-    var indicator = document.getElementById('valid_indicator');
-
-    // Not valid
-    if (errors.length) {
-      indicator.style.color = 'red';
-      indicator.textContent = "not valid";
-    }
-    // Valid
-    else {
-      indicator.style.color = 'green';
-      indicator.textContent = "valid";
-    }
+      // Valid
+      else {
+        indicator.style.color = 'green';
+        indicator.textContent = "valid";
+      }
+    });
   });
 });
-
-
-
 
 // Hook up the Restore to Default button
 document.getElementById('restore').addEventListener('click', function () {
@@ -100,25 +96,32 @@ document.getElementById('enable_disable').addEventListener('click', function () 
 
 function updateData() {
   var saveData = editor.getValue();
-  
+
   var myJSON = JSON.stringify(saveData);
-  console.log(myJSON)
+  console.log(myJSON);
   var encodedString = btoa(myJSON);
-  console.log(encodedString)
+  json = {
+    "content": encodedString,
+    "message": `${repo} by ${owner} at ${Date.now()}`,
+    "committer": {name: "jr638091", email:"jr638091@gmail.com"},
+  };
   save = $.ajax({
     method: 'PUT',
-    url: `https://api.github.com/repos/${owner}/${repo}/contents/resource/dataset.json`,
-    xhrFields: {
-      withCredentials: true
-    },
+    url: `https://api.github.com/repos/${owner}/${repo}/contents/resource/createtest.json`,
+    // beforeSend: function (request) {
+    //   request.setRequestHeader("Authorization", `token ${access_token}`);
+    // },
     headers: {
-      Authorization: `${access_token} OAUTH-TOKEN`
+      Authorization: `Token ${access_token}`
+      // sha: dataset_info.sha
     },
-    data: {
-      content: encodedString,
-      message: `${repo} by ${owner} at ${Date.now()}`,
-      sha: dataset_info.content
-    }
+    beforeSend: function(x) {
+      if (x && x.overrideMimeType) {
+        x.overrideMimeType("application/j-son;charset=UTF-8");
+      }
+    },
+    dataType: "json",
+    data: JSON.stringify(json)
     // pr = $.ajax({
     //   method: 'POST',
     //   url: `https://api.github.com/repos/${owner}/${repo}/pulls`,
@@ -129,5 +132,8 @@ function updateData() {
     //     Authorization: `${access_token} OAUTH-TOKEN`
     //   },
     // });
+  });
+  save.fail(function (err) {
+    console.log(err);
   });
 }
